d966a26d7923108fbf6b5ab8a0e83fe07848df01
InnaAlieva <alievainna123@gmail.com>
2025-12-05 23:39:00 +0300

    Update e2e.server.js

--- e2e/e2e.server.js
+++ e2e/e2e.server.js
@@ -1,19 +1,33 @@
-const Webpack = require('webpack');
+const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-
 const config = require('../webpack.dev');
 
 const PORT = 8087;
 
-const compiler = Webpack(config);
+const compiler = webpack(config);
+
+// Правильная сборка опций для devServer
 const devServerOptions = {
- ...config.devServer, open: false, port: PORT
+  ...config.devServer, // Распространяем настройки из webpack.dev.js
+  open: false,
+  port: PORT
 };
-const server = new WebpackDevServer(devServerOptions, compiler);
 
 const runServer = async () => {
-  console.log('Starting server on port ' + PORT);
-  await server.start();
+  try {
+    // Создаём сервер
+    const server = new WebpackDevServer(devServerOptions, compiler);
+
+    console.log(`Starting server on port ${PORT}`);
+
+    // Запускаем сервер
+    await server.start();
+
+    console.log('Server is running!');
+  } catch (err) {
+    console.error('Failed to start server:', err);
+    process.exit(1);
+  }
 };
 
 runServer();
