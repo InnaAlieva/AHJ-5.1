a00b6fac82c9e1e59b9cbfc12b76bdb2cd58c323
InnaAlieva <alievainna123@gmail.com>
2025-12-05 23:40:19 +0300

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,10 +6,21 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    static: './public', // Укажите путь к статическим файлам
     historyApiFallback: true,
     open: true,
     compress: true,
     port: 9000,
+    client: {
+      logging: 'info',
+      overlay: true, // Показывает ошибки в браузере
+    },
+    watchFiles: ['src/**/*', 'public/**/*'], // Отслеживает изменения
   },
-  plugins: [ new webpack.HotModuleReplacementPlugin() ],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  performance: {
+    hints: false, // Отключает предупреждения о размере бандла
+  },
 });
